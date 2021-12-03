85207146bec91a35ab344eda451844834badbfc1
Kurai András <k.andras9642@gmail.com>
2021-12-03 13:50:51 +0100

    day 3 part 1 done

--- Years/2021/Day3/Program.cs
+++ Years/2021/Day3/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static Common.Helper;
+using static System.Console;
+
+var lines = await LoadPartLines(1);
+
+var inputWidth = lines[0].Length;
+
+var oneCounts = new int[inputWidth];
+
+foreach (var line in lines)
+{
+    for (var i = 0; i < line.Length; i++)
+    {
+        if (line[i] == '1')
+        {
+            oneCounts[i] = oneCounts[i] + 1;
+        }
+    }
+}
+
+var gammaRateBinary = oneCounts.Aggregate(string.Empty, (sum, ones) => sum + (ones > lines.Length / 2 ? "1" : "0"));
+var gammaRate = Convert.ToInt32(gammaRateBinary, 2);
+
+var epsilonRate = ~gammaRate & (((int)Math.Pow(2, inputWidth)) - 1);
+var epsilonRateBinary = Convert.ToString(epsilonRate, 2);
+
+WriteLine($"GammaRate binary:\t{gammaRateBinary}, decimal:\t{gammaRate}");
+WriteLine($"EpsilonRate binary:\t{epsilonRateBinary}, decimal:\t{epsilonRate}");
+Answer(1, gammaRate * epsilonRate);

35bc7e8c0bf4af16479ec99e9afd2dba69079dd9
Kurai András <k.andras9642@gmail.com>
2021-12-03 11:04:21 +0100

    day1 part 1 done

---
+++ Common/Helper.cs
@@ -0,0 +1,7 @@
+﻿namespace Common;
+
+public static class Helper
+{
+    public static async Task<string> LoadDay(int index) => await File.ReadAllTextAsync($"Data{index}.txt");
+    public static async Task<string[]> LoadDayLines(int index) => await File.ReadAllLinesAsync($"Data{index}.txt");
+}
--- Years/2021/Day1/Program.cs
+++ Years/2021/Day1/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using static Common.Helper;
+
+var numbers = (await LoadDayLines(1))
+    .Select(line => int.Parse(line))
+    .ToArray();
+
+var incrementCount = numbers
+    .Skip(1)
+    .Where((number, index) => number > numbers[index])
+    .Count();
+
+Console.WriteLine($"Part 1 is: {incrementCount}");

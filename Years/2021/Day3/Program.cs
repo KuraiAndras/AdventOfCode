fa27a0e110ce73b6e7acb1821e3909fd0a7dc9f8
Kurai András <k.andras9642@gmail.com>
2021-12-03 16:24:11 +0100

    day 3 part 2 done

--- Years/2021/Day3/Program.cs
+++ Years/2021/Day3/Program.cs
@@ -1,4 +1,5 @@
-﻿using static Common.Helper;
+﻿using System.Collections.Immutable;
+using static Common.Helper;
 using static System.Console;
 
 var lines = await LoadPartLines(1);
@@ -24,6 +25,36 @@ var gammaRate = Convert.ToInt32(gammaRateBinary, 2);
 var epsilonRate = ~gammaRate & (((int)Math.Pow(2, inputWidth)) - 1);
 var epsilonRateBinary = Convert.ToString(epsilonRate, 2);
 
-WriteLine($"GammaRate binary:\t{gammaRateBinary}, decimal:\t{gammaRate}");
-WriteLine($"EpsilonRate binary:\t{epsilonRateBinary}, decimal:\t{epsilonRate}");
+WriteLine($"Gamma rate binary:\t{gammaRateBinary}, decimal:\t{gammaRate}");
+WriteLine($"Epsilon rate binary:\t{epsilonRateBinary}, decimal:\t{epsilonRate}");
 Answer(1, gammaRate * epsilonRate);
+
+WriteLine();
+
+var oxygenRateBinary = FindRate(lines, inputWidth, new[] { '1', '0' });
+var co2RateBinary = FindRate(lines, inputWidth, new[] { '0', '1' });
+
+var oxygenRate = Convert.ToInt32(oxygenRateBinary, 2);
+var co2Rate = Convert.ToInt32(co2RateBinary, 2);
+
+WriteLine($"Oxygen rate binary:\t{oxygenRateBinary}, decimal:\t{oxygenRate}");
+WriteLine($"CO2 rate binary:\t{co2RateBinary}, decimal:\t{co2Rate}");
+Answer(2, oxygenRate * co2Rate);
+
+static string FindRate(ImmutableArray<string> lines, int inputWidth, char[] characterPriority)
+{
+    var currentBit = 0;
+
+    while (lines.Length != 1)
+    {
+        var ones = lines.Count(line => line[currentBit] == '1');
+
+        var fliterChar = ones >= lines.Length / 2f ? characterPriority[0] : characterPriority[1];
+
+        lines = lines.Where(line => line[currentBit] == fliterChar).ToImmutableArray();
+
+        currentBit++;
+    }
+
+    return lines[0];
+}

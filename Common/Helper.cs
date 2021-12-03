f7c19d3439edda582cf08dfd1014d1774a878d4c
Kurai András <k.andras9642@gmail.com>
2021-12-03 12:50:16 +0100

    use immutable arrays in common

--- Common/Helper.cs
+++ Common/Helper.cs
@@ -1,11 +1,12 @@
-﻿using static System.Console;
+﻿using System.Collections.Immutable;
+using static System.Console;
 
 namespace Common;
 
 public static class Helper
 {
     public static async Task<string> LoadPart(int index) => await File.ReadAllTextAsync($"Data{index}.txt");
-    public static async Task<string[]> LoadPartLines(int index) => await File.ReadAllLinesAsync($"Data{index}.txt");
+    public static async Task<ImmutableArray<string>> LoadPartLines(int index) => (await File.ReadAllLinesAsync($"Data{index}.txt")).ToImmutableArray();
 
     public static void Answer<T>(int part, T answer) => WriteLine($"Part {part} answer is: {answer}");
 }

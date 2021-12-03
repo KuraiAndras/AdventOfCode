5239990a0cced220c61d95ed8a25528bc04e39a0
Kurai András <k.andras9642@gmail.com>
2021-12-03 12:47:48 +0100

    day 2 part 2 done

--- Years/2021/Day2/Program.cs
+++ Years/2021/Day2/Program.cs
@@ -1,4 +1,5 @@
-﻿using static Common.Helper;
+﻿using System.Collections.Immutable;
+using static Common.Helper;
 using static System.Console;
 
 var lines = await LoadPartLines(1);
@@ -9,7 +10,7 @@ var directions = lines
         var parts = d.Split(' ');
         return (parts[0], int.Parse(parts[1]));
     })
-    .ToArray();
+    .ToImmutableArray();
 
 var horizontal = 0;
 var vertical = 0;
@@ -33,5 +34,34 @@ foreach (var direction in directions)
 var answer = horizontal * vertical;
 
 WriteLine($"Horizontal: {horizontal}, Vertical: {vertical}");
-
 Answer(1, answer);
+
+horizontal = 0;
+vertical = 0;
+
+var aim = 0;
+
+foreach (var direction in directions)
+{
+    var currentHorizontal = direction switch
+    {
+        ("forward", var value) => value,
+        _ => 0,
+    };
+
+    horizontal += currentHorizontal;
+
+    aim += direction switch
+    {
+        ("down", var value) => value,
+        ("up", var value) => -value,
+        _ => 0,
+    };
+
+    vertical += aim * currentHorizontal;
+}
+
+answer = horizontal * vertical;
+
+WriteLine($"Horizontal: {horizontal}, Vertical: {vertical}");
+Answer(2, answer);

eb7d60490d60a10821d814d66205058a3376214a
Kurai András <k.andras9642@gmail.com>
2021-12-05 10:11:07 +0100

    use tabs in answer output

--- Common/Helper.cs
+++ Common/Helper.cs
@@ -8,5 +8,5 @@ public static class Helper
     public static async Task<string> LoadPart(int index) => await File.ReadAllTextAsync($"Data{index}.txt");
     public static async Task<ImmutableArray<string>> LoadPartLines(int index) => (await File.ReadAllLinesAsync($"Data{index}.txt")).ToImmutableArray();
 
-    public static void Answer<T>(int part, T answer) => WriteLine($"Part {part} answer is: {answer}");
+    public static void Answer<T>(int part, T answer) => WriteLine($"Part\t{part} answer is:\t{answer}");
 }

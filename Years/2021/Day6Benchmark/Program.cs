be2920a93130167ddc0be1c6e7930457e2ea0b86
Kurai András <k.andras9642@gmail.com>
2021-12-06 10:42:12 +0100

    add day 6 benchmark

---
+++ Years/2021/Day6Benchmark/Program.cs
@@ -0,0 +1,22 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using Day6;
+using System.Numerics;
+
+_ = BenchmarkRunner.Run<Bench>();
+
+[MemoryDiagnoser]
+public class Bench
+{
+    private List<int> _numbers = new List<int>();
+
+    [GlobalSetup]
+    public void Setup() => _numbers = File
+        .ReadAllText("Data1.txt")
+        .Split(',')
+        .Select(int.Parse)
+        .ToList();
+
+    [Benchmark]
+    public BigInteger Simulate() => LanternfishSimulation.Simulate(_numbers, 256, 9, 7);
+}

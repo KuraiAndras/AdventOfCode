f6349bf97368df6f15099a188749b4c8266af7f6
Kurai András <k.andras9642@gmail.com>
2021-12-07 09:12:53 +0100

    day 7 part 1 done

--- Years/2021/Day7/Program.cs
+++ Years/2021/Day7/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static Common.Helper;
+
+var input = (await LoadPart(1)).Split(',').Select(int.Parse).ToArray();
+
+var maxValue = input.Max();
+var minValue = input.Min();
+
+var answer1 = Enumerable.Range(minValue, maxValue)
+    .Select(x => input.Aggregate(0, (sum, current) => sum + Math.Abs(current - x)))
+    .Min();
+
+Answer(1, answer1);

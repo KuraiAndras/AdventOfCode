5eabeb801383f8be59a3ff6cbd3f63909bc0fa0e
Kurai András <k.andras9642@gmail.com>
2021-12-08 09:27:13 +0100

    day 8 part 1 done

--- Years/2021/Day8/Program.cs
+++ Years/2021/Day8/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Collections.Immutable;
+using static Common.Helper;
+
+var inputLines = await LoadPartLines(1);
+
+var input = inputLines
+    .Select(l => l.Split(" | "))
+    .Select(i => (signals: i[0].Split(' '), segments: i[1].Split(' ')))
+    .ToImmutableArray();
+
+var answer1 = input
+    .SelectMany(i => i.segments)
+    .Count(segment => segment.Length is 2 or 4 or 3 or 7);
+
+Answer(1, answer1);

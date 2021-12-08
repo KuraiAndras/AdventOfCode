3afba7c878be88534d80d6cb0e680748eebb0db2
Kurai András <k.andras9642@gmail.com>
2021-12-08 11:41:52 +0100

    clean up day 8 part 2

--- Years/2021/Day8/Program.cs
+++ Years/2021/Day8/Program.cs
@@ -34,44 +34,44 @@ static int GetSegmentValue(SegmentData segmentData)
     var four = segmentData.Wirings.Single(w => MatchEasyValue(w) is 4);
     var one = segmentData.Wirings.Single(w => MatchEasyValue(w) is 1);
 
-    var a = seven.Except(one).First();
+    var a = seven.Except(one).Single();
     var bd = four.Except(one);
 
     var three = segmentData.Wirings.Single(w => w.Length == 5 && one.All(c => w.Contains(c)));
 
     var dg = three.Except(seven);
 
-    var d = bd.Intersect(dg).First();
+    var d = bd.Intersect(dg).Single();
 
-    var b = bd.Except(new[] { d }).First();
+    var b = bd.Except(d).Single();
 
-    var g = dg.Except(new[] { d }).First();
+    var g = dg.Except(d).Single();
 
     var zero = segmentData.Wirings.Single(w => !w.Contains(d) && w != one && w != seven);
 
-    var e = zero.Except(seven).Except(new[] { b, g }).First();
+    var e = zero.Except(seven).Except(b, g).Single();
 
     var two = segmentData.Wirings.Single(w => w.Length == 5 && w.Contains(e));
 
-    var c = two.Except(new[] { a, d, e, g }).First();
+    var c = two.Except(a, d, e, g).Single();
 
-    var f = one.Except(new[] { c }).First();
+    var f = one.Except(c).Single();
 
     return int.Parse(segmentData.Segments.Aggregate(string.Empty, (sum, segment) => sum + MatchNumber(segment, a, b, c, d, e, f, g).ToString()));
 }
 
 static int MatchNumber(string segment, char a, char b, char c, char d, char e, char f, char g)
 {
-    if (segment.SetEquals(new[] { a, b, c, e, f, g })) return 0;
-    if (segment.SetEquals(new[] { c, f })) return 1;
-    if (segment.SetEquals(new[] { a, c, d, e, g })) return 2;
-    if (segment.SetEquals(new[] { a, c, d, f, g })) return 3;
-    if (segment.SetEquals(new[] { b, c, d, f })) return 4;
-    if (segment.SetEquals(new[] { a, b, d, f, g })) return 5;
-    if (segment.SetEquals(new[] { a, b, d, e, f, g })) return 6;
-    if (segment.SetEquals(new[] { a, c, f })) return 7;
-    if (segment.SetEquals(new[] { a, b, c, d, e, f, g })) return 8;
-    if (segment.SetEquals(new[] { a, b, c, d, f, g })) return 9;
+    if (segment.SetEquals(a, b, c, e, f, g)) return 0;
+    if (segment.SetEquals(c, f)) return 1;
+    if (segment.SetEquals(a, c, d, e, g)) return 2;
+    if (segment.SetEquals(a, c, d, f, g)) return 3;
+    if (segment.SetEquals(b, c, d, f)) return 4;
+    if (segment.SetEquals(a, b, d, f, g)) return 5;
+    if (segment.SetEquals(a, b, d, e, f, g)) return 6;
+    if (segment.SetEquals(a, c, f)) return 7;
+    if (segment.SetEquals(a, b, c, d, e, f, g)) return 8;
+    if (segment.SetEquals(a, b, c, d, f, g)) return 9;
 
     throw new InvalidCastException($"Could not match segment {segment}");
 }
@@ -81,5 +81,7 @@ record SegmentData(string[] Wirings, string[] Segments);
 [System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1050:Declare types in namespaces", Justification = "Nicer")]
 public static class EnumerableExtensions
 {
-    public static bool SetEquals<T>(this IEnumerable<T> source, IEnumerable<T> other) => new HashSet<T>(source).SetEquals(other);
+    public static bool SetEquals<T>(this IEnumerable<T> source, params T[] other) => new HashSet<T>(source).SetEquals(other);
+
+    public static IEnumerable<T> Except<T>(this IEnumerable<T> source, params T[] elements) => Enumerable.Except(source, elements);
 }

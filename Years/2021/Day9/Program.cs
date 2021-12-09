d25f80c0f0091484e2f2f201a8e4dcdf6149481d
Kurai András <k.andras9642@gmail.com>
2021-12-09 11:13:20 +0100

    day 9 part 2 done

--- Years/2021/Day9/Program.cs
+++ Years/2021/Day9/Program.cs
@@ -35,3 +35,49 @@ for (var i = 0; i < height; i++)
 var answer1 = lowPoints.Aggregate(0, (sum, current) => sum + current + 1);
 
 Answer(1, answer1);
+
+var map = input.Select(i => i.ToArray()).ToArray();
+var basins = new List<List<(int y, int x)>>();
+
+for (var i = 0; i < height; i++)
+{
+    for (var j = 0; j < width; j++)
+    {
+        var current = input[i][j];
+
+        if (current == 9) continue;
+
+        if (basins.Any(b => b.Contains((i, j)))) continue;
+
+        var basin = new List<(int y, int x)>();
+
+        FloodFill(map, j, i, height, width, basin);
+
+        if (basin.Count > 0) basins.Add(basin);
+    }
+}
+
+var answer2 = basins.OrderByDescending(b => b.Count).Take(3).Aggregate(1, (product, basin) => product * basin.Count);
+
+Answer(2, answer2);
+
+static void FloodFill(int[][] map, int x, int y, int height, int width, List<(int y, int x)> basin)
+{
+    if (x < 0 || x >= width || y < 0 || y >= height) return;
+    if (map[y][x] == 9) return;
+
+    var currentCoordinate = (y, x);
+    if (!basin.Contains(currentCoordinate))
+    {
+        basin.Add(currentCoordinate);
+    }
+    else
+    {
+        return;
+    }
+
+    FloodFill(map, x + 1, y, height, width, basin);
+    FloodFill(map, x - 1, y, height, width, basin);
+    FloodFill(map, x, y + 1, height, width, basin);
+    FloodFill(map, x, y - 1, height, width, basin);
+}

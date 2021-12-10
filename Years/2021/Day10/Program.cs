fd099c6557bde57fd54967d6c18110c84006108d
Kurai András <k.andras9642@gmail.com>
2021-12-10 12:40:39 +0100

    day 10 part 1 done

--- Years/2021/Day10/Program.cs
+++ Years/2021/Day10/Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static Common.Helper;
+
+var lines = await LoadPartLines(1);
+
+var firstIllegalCharacters = new Dictionary<char, int>
+{
+    { ')', 0 },
+    { ']', 0 },
+    { '}', 0 },
+    { '>', 0 },
+};
+foreach (var line in lines)
+{
+    var openBrackets = new Stack<char>();
+    foreach (var current in line)
+    {
+        if (current is '(' or '[' or '{' or '<')
+        {
+            openBrackets.Push(current);
+        }
+        else
+        {
+            var top = openBrackets.Pop();
+            if (top is '(' && current is not ')'
+                || top is '[' && current is not ']'
+                || top is '{' && current is not '}'
+                || top is '<' && current is not '>')
+            {
+                firstIllegalCharacters[current]++;
+
+                break;
+            }
+        }
+    }
+}
+
+var answer1 = 0;
+foreach (var key in firstIllegalCharacters.Keys)
+{
+    var count = firstIllegalCharacters[key];
+    answer1 += key switch
+    {
+        ')' => 3 * count,
+        ']' => 57 * count,
+        '}' => 1197 * count,
+        '>' => 25137 * count,
+        _ => 0,
+    };
+}
+
+Answer(1, answer1);

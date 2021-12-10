b08eb17e400d1940b7d6cebc7a22e89920e8a2aa
Kurai András <k.andras9642@gmail.com>
2021-12-10 13:05:51 +0100

    day 10 part 2 done

--- Years/2021/Day10/Program.cs
+++ Years/2021/Day10/Program.cs
@@ -9,9 +9,14 @@ var firstIllegalCharacters = new Dictionary<char, int>
     { '}', 0 },
     { '>', 0 },
 };
+
+var nonCorroptLines = new List<string>();
 foreach (var line in lines)
 {
     var openBrackets = new Stack<char>();
+
+    var hadError = false;
+
     foreach (var current in line)
     {
         if (current is '(' or '[' or '{' or '<')
@@ -28,10 +33,14 @@ foreach (var line in lines)
             {
                 firstIllegalCharacters[current]++;
 
+                hadError = true;
+
                 break;
             }
         }
     }
+
+    if (!hadError) nonCorroptLines.Add(line);
 }
 
 var answer1 = 0;
@@ -49,3 +58,47 @@ foreach (var key in firstIllegalCharacters.Keys)
 }
 
 Answer(1, answer1);
+
+var scores = new List<long>();
+foreach (var line in nonCorroptLines)
+{
+    var openBrackets = new Stack<char>();
+
+    foreach (var current in line)
+    {
+        if (current is '(' or '[' or '{' or '<')
+        {
+            openBrackets.Push(current);
+        }
+        else
+        {
+            openBrackets.Pop();
+        }
+    }
+
+    var score = 0L;
+    while (openBrackets.Count > 0)
+    {
+        var current = openBrackets.Pop();
+
+        score *= 5L;
+
+        score += current switch
+        {
+            '(' => 1L,
+            '[' => 2L,
+            '{' => 3L,
+            '<' => 4L,
+            _ => 0L,
+        };
+    }
+
+    scores.Add(score);
+}
+
+scores = scores.OrderBy(x => x).ToList();
+
+var middle = (scores.Count / 2);
+var answer = scores[middle];
+
+Answer(2, answer);

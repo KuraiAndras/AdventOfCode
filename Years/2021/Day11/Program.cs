2195f267637fed3a664cea12261127edd096008d
Kurai András <k.andras9642@gmail.com>
2021-12-11 09:58:22 +0100

    day 11 part 2 done

--- Years/2021/Day11/Program.cs
+++ Years/2021/Day11/Program.cs
@@ -6,65 +6,18 @@ var lines = await LoadPartLines(1);
 
 var numberRegex = new Regex(@"\d", RegexOptions.Compiled, TimeSpan.FromSeconds(1));
 
-var energyLeves = lines
+var energyLevels = lines
     .Select(line => numberRegex.Matches(line).Select(m => int.Parse(m.Value)).ToImmutableArray())
     .ToImmutableArray();
 
 var height = lines.Length;
 var width = lines[0].Length;
 
-var map = new Octopus[height][];
-
-for (var i = 0; i < height; i++)
-{
-    map[i] = new Octopus[width];
-
-    for (var j = 0; j < width; j++)
-    {
-        map[i][j] = new Octopus(j, i, energyLeves[i][j], false);
-    }
-}
-
-const int numberOfSteps = 100;
-
-var flashCount = 0;
-
-for (var s = 0; s < numberOfSteps; s++)
-{
-    for (var i = 0; i < height; i++)
-    {
-        for (var j = 0; j < width; j++)
-        {
-            var current = map[i][j];
-
-            if (current.DidFlash) continue;
-
-            FloodFill(map, j, i, height, width, ref flashCount);
-        }
-    }
-
-    Console.WriteLine();
-
-    Console.WriteLine($"Step: {s + 1}");
-
-    for (var i = 0; i < height; i++)
-    {
-        for (var j = 0; j < width; j++)
-        {
-            var current = map[i][j];
-
-            current.DidFlash = false;
-
-            Console.Write(current.EnergyLevel);
-        }
-
-        Console.WriteLine();
-    }
-
-    Console.WriteLine();
-}
+var flashCount = CalculateFlashes(height, width, energyLevels);
+var firstFlash = CalculateFirstFlash(height, width, energyLevels);
 
 Answer(1, flashCount);
+Answer(2, firstFlash);
 
 static void FloodFill(Octopus[][] map, int x, int y, int height, int width, ref int flashCount)
 {
@@ -91,6 +44,124 @@ static void FloodFill(Octopus[][] map, int x, int y, int height, int width, ref 
     FloodFill(map, x - 1, y - 1, height, width, ref flashCount);
 
     current.EnergyLevel = 0;
+}
+
+static int CalculateFlashes(int height, int width, ImmutableArray<ImmutableArray<int>> energyLevels)
+{
+    var map = new Octopus[height][];
+
+    for (var i = 0; i < height; i++)
+    {
+        map[i] = new Octopus[width];
+
+        for (var j = 0; j < width; j++)
+        {
+            map[i][j] = new Octopus(j, i, energyLevels[i][j], false);
+        }
+    }
+
+    const int numberOfSteps = 100;
+
+    var flashCount = 0;
+    for (var s = 0; s < numberOfSteps; s++)
+    {
+        for (var i = 0; i < height; i++)
+        {
+            for (var j = 0; j < width; j++)
+            {
+                var current = map[i][j];
+
+                if (current.DidFlash) continue;
+
+                FloodFill(map, j, i, height, width, ref flashCount);
+            }
+        }
+
+        Console.WriteLine();
+
+        Console.WriteLine($"Step: {s + 1}");
+
+        for (var i = 0; i < height; i++)
+        {
+            for (var j = 0; j < width; j++)
+            {
+                var current = map[i][j];
+
+                current.DidFlash = false;
+
+                Console.Write(current.EnergyLevel);
+            }
+
+            Console.WriteLine();
+        }
+
+        Console.WriteLine();
+    }
+
+    return flashCount;
+}
+
+static int CalculateFirstFlash(int height, int width, ImmutableArray<ImmutableArray<int>> energyLevels)
+{
+    var map = new Octopus[height][];
+
+    for (var i = 0; i < height; i++)
+    {
+        map[i] = new Octopus[width];
+
+        for (var j = 0; j < width; j++)
+        {
+            map[i][j] = new Octopus(j, i, energyLevels[i][j], false);
+        }
+    }
+
+    var flashCount = 0;
+    var numberOfFishes = height * width;
+
+    var s = 0;
+    var zeroCount = 0;
+    while (zeroCount != numberOfFishes)
+    {
+        for (var i = 0; i < height; i++)
+        {
+            for (var j = 0; j < width; j++)
+            {
+                var current = map[i][j];
+
+                if (current.DidFlash) continue;
+
+                FloodFill(map, j, i, height, width, ref flashCount);
+            }
+        }
+
+        Console.WriteLine();
+
+        Console.WriteLine($"Step: {s + 1}");
+
+        zeroCount = 0;
+
+        for (var i = 0; i < height; i++)
+        {
+            for (var j = 0; j < width; j++)
+            {
+                var current = map[i][j];
+
+                current.DidFlash = false;
+
+                if (current.EnergyLevel == 0) zeroCount++;
+
+                Console.Write(current.EnergyLevel);
+            }
+
+            Console.WriteLine();
+        }
+
+        Console.WriteLine();
+
+        s++;
+    }
+
+    return s;
 }
 
 class Octopus

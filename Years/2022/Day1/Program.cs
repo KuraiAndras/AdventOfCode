d8039a8caf7a03fe11a2f96a85e088a1ea5be5a8
Kurai András <k.andras9642@gmail.com>
2022-12-01 06:29:49 +0100

    day 1 part 1

--- Years/2022/Day1/Program.cs
+++ Years/2022/Day1/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using MoreLinq;
+using System.Collections.Immutable;
+using static Common.Helper;
+using static System.Console;
+
+var biggestCalorie = (await LoadPartLines(1))
+    .Split(string.Empty)
+    .Select(g => g.Select(int.Parse).ToImmutableArray())
+    .ToImmutableArray()
+    .Max(g => g.Aggregate(0, (sum, c) => sum + c));
+
+WriteLine($"part 1: {biggestCalorie}");

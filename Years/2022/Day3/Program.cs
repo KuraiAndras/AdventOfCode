c8e0154b46996b501bd9fa614ec6fc92986c1ff9
Kurai András <k.andras9642@gmail.com>
2022-12-03 09:55:57 +0100

    implement day 3 part 2

--- Years/2022/Day3/Program.cs
+++ Years/2022/Day3/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Immutable;
+﻿using MoreLinq;
+using System.Collections.Immutable;
 
 var rucksacks = await LoadPartLines(1);
 
@@ -10,7 +11,22 @@ var duplicatesPerRucksack = rucksacksWithCompartments
     .Select(rucksack => rucksack.first.Intersect(rucksack.second).ToImmutableArray())
     .ToImmutableArray();
 
-var answer1 = duplicatesPerRucksack
-    .Aggregate(0, (sum, c) => sum + c.Aggregate(0, (sumVal, cVal) => cVal >= 'a' ? sumVal + (cVal - 'a' + 1) : sumVal + (cVal - 'A' + 27)));
+var answer1 = AggregateCommonItems(duplicatesPerRucksack);
 
 Answer(1, answer1);
+
+var threeElfGroups = rucksacks
+    .Batch(3)
+    .Select(b => b.ToImmutableArray())
+    .ToImmutableArray();
+
+var groupCommonItems = threeElfGroups
+    .Select(g => g[0].Intersect(g[1]).Intersect(g[2]).ToImmutableArray())
+    .ToImmutableArray();
+
+var answer2 = AggregateCommonItems(groupCommonItems);
+
+Answer(2, answer2);
+
+static int AggregateCommonItems(ImmutableArray<ImmutableArray<char>> itemsPerGroup) => itemsPerGroup
+    .Aggregate(0, (sum, c) => sum + c.Aggregate(0, (sumVal, cVal) => cVal >= 'a' ? sumVal + (cVal - 'a' + 1) : sumVal + (cVal - 'A' + 27)));

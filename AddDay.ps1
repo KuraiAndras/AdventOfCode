12463e586e565569d0df374f9edda47f07b18a71
Kurai András <k.andras9642@gmail.com>
2022-12-03 09:17:34 +0100

    fix addday

--- AddDay.ps1
+++ AddDay.ps1
@@ -11,12 +11,6 @@ $ProjectFolder = "${CurrentDirectory}\Years\${Year}\Day${Day}"
 $InputFile = "${ProjectFolder}\Data1.txt"
 $ProjectFile = "${ProjectFolder}\Day${Day}.csproj"
 
-Write-Host "Downloading input"
-
-$AuthCookie = Get-Content ".\cookie.txt"
-
-Invoke-WebRequest -Uri "https://adventofcode.com/${Year}/day/${Day}/input" -Headers @{"cookie" = "session=${AuthCookie}" } -OutFile $InputFile
-
 Write-Host("Creating project at path: ${ProjectFile}")
 
 if (Test-Path $ProjectFile) {
@@ -31,3 +25,9 @@ if (-Not (Test-Path -Path $ProjectFolder)) {
 dotnet new console -o $ProjectFolder
 
 dotnet sln add $ProjectFolder
+
+Write-Host "Downloading input"
+
+$AuthCookie = Get-Content ".\cookie.txt"
+
+Invoke-WebRequest -Uri "https://adventofcode.com/${Year}/day/${Day}/input" -Headers @{"cookie" = "session=${AuthCookie}" } -OutFile $InputFile

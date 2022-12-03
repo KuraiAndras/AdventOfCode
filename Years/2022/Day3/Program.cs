44fda2ea61a802c2ca9e3a74e1c9b65659c92746
Kurai András <k.andras9642@gmail.com>
2022-12-03 09:40:18 +0100

    implement day 3 part 1

--- Years/2022/Day3/Program.cs
+++ Years/2022/Day3/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Collections.Immutable;
+
+var rucksacks = await LoadPartLines(1);
+
+var rucksacksWithCompartments = rucksacks
+    .Select(rucksack => (first: rucksack.Take(rucksack.Length / 2).ToImmutableArray(), second: rucksack.Skip(rucksack.Length / 2).ToImmutableArray()))
+    .ToImmutableArray();
+
+var duplicatesPerRucksack = rucksacksWithCompartments
+    .Select(rucksack => rucksack.first.Intersect(rucksack.second).ToImmutableArray())
+    .ToImmutableArray();
+
+var answer1 = duplicatesPerRucksack
+    .Aggregate(0, (sum, c) => sum + c.Aggregate(0, (sumVal, cVal) => cVal >= 'a' ? sumVal + (cVal - 'a' + 1) : sumVal + (cVal - 'A' + 27)));
+
+Answer(1, answer1);

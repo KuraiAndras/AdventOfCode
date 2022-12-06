946ab3153be6ffc2774fdef6f397a57290d8725f
Kurai András <k.andras9642@gmail.com>
2022-12-06 17:47:27 +0100

    implement part 1

--- Years/2022/Day6/Program.cs
+++ Years/2022/Day6/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var input = await LoadPart(1);
+
+var messageBeginBuffer = new Queue<char>(input.Take(4));
+int messageBeginEndIndex = 4;
+
+for (var i = 4; i < input.Length; i++)
+{
+    messageBeginEndIndex = i;
+    var set = new HashSet<char>(messageBeginBuffer);
+    if (set.Count == messageBeginBuffer.Count) break;
+
+    messageBeginBuffer.Dequeue();
+    messageBeginBuffer.Enqueue(input[messageBeginEndIndex]);
+}
+
+Answer(1, messageBeginEndIndex);
+

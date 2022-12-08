a4c31f0f0dc261c9d44a169392b138206767b81f
Kurai András <k.andras9642@gmail.com>
2022-12-08 07:03:18 +0100

    implement part 1

--- Years/2022/Day8/Program.cs
+++ Years/2022/Day8/Program.cs
@@ -1,2 +1,61 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var map = (await LoadPartLines(1))
+    .Select(l => l.Select(c => int.Parse(new[] { c })).ToArray())
+    .ToArray();
+
+var width = map[0].Length;
+var height = map.Length;
+
+var visibleCount = 0;
+for (var y = 0; y < height; y++)
+{
+    for (var x = 0; x < width; x++)
+    {
+        visibleCount += IsVisible(map, x, y, width, height) ? 1 : 0;
+    }
+}
+
+Answer(1, visibleCount);
+
+static bool IsVisible(int[][] map, int x, int y, int width, int height)
+{
+    var currentHeight = map[y][x];
+
+    if (x == 0 || x == width) return true;
+    if (y == 0 || y == height) return true;
+
+    var canSeeFromLeft = true;
+    for (var i = x - 1; i >= 0; i--)
+    {
+        canSeeFromLeft = map[y][i] < currentHeight;
+        if (!canSeeFromLeft) break;
+    }
+    if (canSeeFromLeft) return true;
+
+    var canSeeFromRight = true;
+    for (var i = x + 1; i < width; i++)
+    {
+        canSeeFromRight = map[y][i] < currentHeight;
+        if (!canSeeFromRight) break;
+    }
+    if (canSeeFromRight) return true;
+
+    // top
+    var canSeeFromTop = true;
+    for (var i = y - 1; i >= 0; i--)
+    {
+        canSeeFromTop = map[i][x] < currentHeight;
+        if (!canSeeFromTop) break;
+    }
+    if (canSeeFromTop) return true;
+
+    // bottom
+    var canSeeFromBottom = true;
+    for (var i = y + 1; i < height; i++)
+    {
+        canSeeFromBottom = map[i][x] < currentHeight;
+        if (!canSeeFromBottom) break;
+    }
+    if (canSeeFromBottom) return true;
+
+    return false;
+}

d8231bc412c8fc95dc8a347b2416cb6dba6b59ad
Kurai András <k.andras9642@gmail.com>
2023-12-01 11:10:47 +0100

    day 1 part 1 done

--- Years/2023/Day1/Program.cs
+++ Years/2023/Day1/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var lines = await LoadPartLines(1);
+
+var numberCharacters = new[] { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+
+var numbers = new List<int>();
+foreach (var line in lines)
+{
+    char? firstNumber = null;
+    char? secondNumer = null;
+
+    var i = 0;
+    var j = line.Length -1;
+    while(firstNumber is null || secondNumer is null)
+    {
+        if (firstNumber is null && numberCharacters.Contains(line[i]))
+        {
+            firstNumber = line[i];
+        }
+
+        if (secondNumer is null && numberCharacters.Contains(line[j]))
+        {
+            secondNumer = line[j];
+        }
+
+        i++;
+        j--;
+    }
+
+    if (firstNumber is null || secondNumer is null)
+    {
+        throw new Exception("No numbers found");
+    }
+
+    var finalNumber = int.Parse($"{firstNumber}{secondNumer}");
+    numbers.Add(finalNumber);
+    WriteLine(finalNumber);
+}
+
+var result1 = numbers.Sum();
+
+WriteLine(result1);
+

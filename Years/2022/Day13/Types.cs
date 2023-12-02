a9e95acd9e622c13370df6a1389ba617be2d7fcf
Kurai András <k.andras9642@gmail.com>
2023-12-02 05:52:34 +0100

    asd

---
+++ Years/2022/Day13/Types.cs
@@ -0,0 +1,19 @@
+﻿using OneOf;
+
+namespace Day13;
+
+//public abstract class PacketData
+//{
+
+//}
+//public class IntData : PacketData
+//{
+//    public int Value { get; set; }
+//}
+//public class ListData : PacketData
+//{
+//    public List<PacketData> Value { get; set; }
+//}
+
+[GenerateOneOf]
+public partial class PacketData : OneOfBase<int, List<PacketData>> { }

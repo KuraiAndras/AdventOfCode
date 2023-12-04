7f26da7f879cdbd5b1b83074fa4908e4e3378dfe
Kurai András <k.andras9642@gmail.com>
2023-12-04 11:43:49 +0100

    asd

--- Years/2023/Day4/Program.cs
+++ Years/2023/Day4/Program.cs
@@ -2,7 +2,7 @@
 
 var cardRegex = CardRegex();
 
-var cards = (await LoadPartLines(1))
+var cards = (await LoadPartLines(0))
     .Select(line =>
     {
         var cardText = cardRegex.Match(line).Value;
@@ -14,14 +14,29 @@ var cards = (await LoadPartLines(1))
 
         return new Card(id, winningNumbers, ownNumbers);
     })
-    .ToArray();
+    .ToList();
 
 var points = cards
     .Select(c => c.OwnNumbers.Count(c.WinningNumbers.Contains))
     .Select(p => p == 0 ? 0 : Math.Pow(2, p - 1))
     .ToArray();
 
+var winningCardsCount = 0;
+for (var i = 0; i < cards.Count; i++)
+{
+    var card = cards[i];
+
+    var winCount = card.WinningNumbers.Count(card.OwnNumbers.Contains);
+    if (winCount == 0) continue;
+
+    for (var j = 1; j < winCount + 1; j++)
+    {
+        var wonCard = cards.Find(c => c.Id == card.Id);
+    }
+}
+
 Answer(0, points.Sum());
+Answer(1, winningCardsCount);
 
 record Card(int Id, IReadOnlyList<int> WinningNumbers, IReadOnlyList<int> OwnNumbers);
 
